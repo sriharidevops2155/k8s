33af0357a0be83129f1ea834eedc1bdacfaedf32
sriharibandi <shreharii@gmail.com>
2025-07-26 20:51:11 +0530

    k8s

--- k8s.tf
+++ k8s.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "k8s" {
   ami           = local.ami_id
   #instance_type = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.allow_all_docker.id]
+  vpc_security_group_ids = [aws_security_group.allow_all_k8s.id]
   instance_type = "t3.medium"
   # need more for terraform
   root_block_device {
