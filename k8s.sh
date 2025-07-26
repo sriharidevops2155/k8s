aeeb544f8b747123b32949417244e27f6a88f79f
sriharibandi <shreharii@gmail.com>
2025-07-26 22:23:42 +0530

    k8s

--- k8s.sh
+++ k8s.sh
@@ -14,23 +14,24 @@ lvextend -L +10G /dev/RootVG/varVol
 xfs_growfs /
 xfs_growfs /var
 
-# RCH=amd64
-# PLATFORM=$(uname -s)_$ARCH
-# curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
-# curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
-# tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
-# install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
+RCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+sleep 2m
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
 
-# curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.0/2025-05-01/bin/linux/amd64/kubectl
-# chmod +x ./kubectl
-# mv kubectl /usr/local/bin/kubectl
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.0/2025-05-01/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv kubectl /usr/local/bin/kubectl
 
-# eksctl version
-# kubectl version
+eksctl version
+kubectl version
 
-# git clone https://github.com/sriharidevops2155/k8s.git
-# cd k8s
-# eksctl create cluster --config-file=eksctl.yaml
+git clone https://github.com/sriharidevops2155/k8s.git
+cd k8s
+eksctl create cluster --config-file=eksctl.yaml
 
 
 # Need to exit once and relogin again to sync the changes
