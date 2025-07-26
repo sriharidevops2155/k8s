eba32fe20a98d9a92790b7948277169cfc525464
sriharibandi <shreharii@gmail.com>
2025-07-26 21:18:57 +0530

    k8s

--- k8s.sh
+++ k8s.sh
@@ -17,6 +17,7 @@ xfs_growfs /var
 # RCH=amd64
 # PLATFORM=$(uname -s)_$ARCH
 # curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+# curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
 # tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
 # install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
 
