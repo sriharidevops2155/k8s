88993a3482ca4c4a614dbd26e7944488e226d94c
sriharibandi <shreharii@gmail.com>
2025-07-27 16:08:54 +0530

    k8s

--- k8s.sh
+++ k8s.sh
@@ -28,6 +28,8 @@ mv kubectl /usr/local/bin/kubectl
 eksctl version
 kubectl version
 
-# git clone https://github.com/sriharidevops2155/k8s.git && cd k8s && eksctl create cluster --config-file=eks.yml
+git clone https://github.com/sriharidevops2155/k8s.git
+
+#cd k8s && eksctl create cluster --config-file=eks.yml
 
 # eksctl delete cluster --config-file=eks.yml
