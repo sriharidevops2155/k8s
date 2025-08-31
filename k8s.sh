ee3388adb646406f58f838e3adff75b7691e3df1
sriharibandi <shreharii@gmail.com>
2025-08-31 12:20:27 +0530

    k8s

--- k8s.sh
+++ k8s.sh
@@ -31,6 +31,7 @@ kubectl version
 git clone https://github.com/sriharidevops2155/k8s.git /home/ec2-user/k8s
 
 git clone https://github.com/ahmetb/kubectx /opt/kubectx
+ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
 ln -s /opt/kubectx/kubens /usr/local/bin/kuben
 
 curl -sS https://webinstall.dev/k9s | bash
